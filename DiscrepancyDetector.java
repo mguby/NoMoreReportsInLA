da5e06e858cee80556d79d2eebc31a6f14b933d5
Mark <mguby814@gmail.com>
2016-05-17 13:53:01 -0500

    commented out unecessary code

--- DiscrepancyDetector.java
+++ DiscrepancyDetector.java
@@ -6,13 +6,12 @@ import java.util.Set;
 
 /**
  * Created by Mark on 5/16/2016.
+ * TODO: Handle names spread across multiple pages
  */
 public class DiscrepancyDetector {
 
-    private static Set<String> names;
-
     public static String getDiscrepancies(HashMap<String, String> impact, HashMap<String, String> mad) {
-        StringBuilder sb = new StringBuilder();
+//        StringBuilder sb = new StringBuilder();
         StringBuilder absences = new StringBuilder();
 
         for (Map.Entry<String, String> entry : impact.entrySet()) {
@@ -25,7 +24,6 @@ public class DiscrepancyDetector {
                 if(!val2.equals(val)) {
 //                    System.out.println(val + " " + val2);
                     absences.append(key + " is " + val + " in IMPACT but " + val2 + " in Monthly Attendance Report\n");
-//                    names.add(key);
                 }
             }
             else if(mad.containsKey(key2)) {
@@ -33,30 +31,28 @@ public class DiscrepancyDetector {
                 if(!val2.equals(val)) {
 //                    System.out.println(val + " " + val2);
                     absences.append(key2 + " is " + val + " in IMPACT but " + val2 + " in Monthly Attendance Report\n");
-//                    names.add(key2);
                 }
             }
-            else {
-                sb.append(key + " is in IMPACT but not in Monthly Attendance Report\n");
-            }
+//            else {
+//                sb.append(key + " is in IMPACT but not in Monthly Attendance Report\n");
+//            }
         }
 
-        for (Map.Entry<String, String> entry : mad.entrySet()) {
-            String key = entry.getKey();
-            String val = entry.getValue();
-
-//            if(impact.containsKey(key) && !names.contains(key)) {
-            if(impact.containsKey(key)) {
-                String val2 = impact.get(key);
-                if(!val2.equals(val)) {
+//        for (Map.Entry<String, String> entry : mad.entrySet()) {
+//            String key = entry.getKey();
+//            String val = entry.getValue();
+//
+//            if(impact.containsKey(key)) {
+//                String val2 = impact.get(key);
+//                if(!val2.equals(val)) {
 //                    System.out.println(val + " " + val2);
 //                    absences.append(key + " is " + val + " in Monthly Attendance Report but " + val2 + " in IMPACT\n");
-                }
-            }
-            else {
-                sb.append(key + " is in Monthly Attendance Report but not in IMPACT\n");
-            }
-        }
+//                }
+//            }
+//            else {
+//                sb.append(key + " is in Monthly Attendance Report but not in IMPACT\n");
+//            }
+//        }
 
 //        return sb.toString() + absences.toString();
         return absences.toString();
