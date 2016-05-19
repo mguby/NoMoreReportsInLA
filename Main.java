8fe8f57a2e997217df68797d1c18c2f91c12b5c9
Mark <mguby814@gmail.com>
2016-05-18 23:48:41 -0500

    Runs all schools at once

--- Main.java
+++ Main.java
@@ -1,29 +1,50 @@
 package com.mark.gubatan;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
-//TODO: Command line UI
 public class Main {
 
-    private static String school = "Hamilton Houston Alternative HS";
+    // Change date before each run
+    private static final String date = "051816";
+
+    // Change downloadPath before first run
+    private static final String downloadPath = "D:/Downloads/";
+
+    private static final String[] schools = {"YCLA", "ADA", "Chatham", "Sullivan", "Latino", "JaneAddams", "CYDI",
+            "OHMC", "Truman", "ECA", "CCA", "ASA", "Campos", "Innovations", "WestTown", "WestSide", "APHS", "Charles"};
 
     public static void main(String[] args) throws IOException {
+        final long startTime = System.currentTimeMillis();
+        for(String cur : schools) {
+            runReports(cur);
+        }
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Execution finished in: " + ((endTime - startTime)/1000.0) + " seconds.");
+    }
 
-        PDFManager pdfManager = new PDFManager();
+    private static void runReports(String cur) {
+        try {
+            PDFManager pdfManager = new PDFManager();
+            //IMPACT
+            pdfManager.setFilePath(downloadPath + "IMPACT_" + cur + "_" + date + ".pdf");
+            String impactPDF = pdfManager.ToText();
+            HashMap<String, String> impact = ImpactParser.parseIMPACT(impactPDF);
 
-        //IMPACT
-        pdfManager.setFilePath("D:/Downloads/IMPACT_Charles_051616.pdf");
-        String impactPDF = pdfManager.ToText();
-        ImpactParser ip = new ImpactParser(school);
-        HashMap<String, String> impact = ip.parseIMPACT(impactPDF);
+            //POWER SCHOOL
+            pdfManager.setFilePath(downloadPath + "Monthly_Attendance_" + cur + "_" + date + ".pdf");
+            String madPDF = pdfManager.ToText();
+            HashMap<String, String> mad = PowerschoolParser.parseMonthlyAttendanceReport(madPDF);
 
-        //POWER SCHOOL
-        pdfManager.setFilePath("D:/Downloads/Monthly_Attendance_Charles_051616.pdf");
-        String madPDF = pdfManager.ToText();
-        HashMap<String, String> mad = PowerschoolParser.parseMonthlyAttendanceReport(madPDF);
+            String differences = DiscrepancyDetector.getDiscrepancies(impact, mad);
 
-        String differences = DiscrepancyDetector.getDiscrepancies(impact, mad);
-        System.out.println(differences);
+            PrintWriter writer = new PrintWriter(cur + "_" + date + ".txt", "UTF-8");
+            writer.println(differences);
+            writer.close();
+        }
+        catch (IOException e) {
+            // Probably due to a file not existing (Does the name match the school in the school list?)
+        }
     }
 
 
