47186a66c8c0f2b46f9fc499cfa13ec60b01daf3
Mark Gubatan <mguby814@gmail.com>
2017-01-20 01:18:52 -0600

    Modify folder structure and sort output alphabetically

--- DiscrepancyDetector.java
+++ DiscrepancyDetector.java
@@ -1,7 +1,6 @@
 package com.mark.gubatan;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Mark on 5/16/2016.
@@ -11,7 +10,7 @@ public class DiscrepancyDetector {
 
     public static String getDiscrepancies(HashMap<String, String> impact, HashMap<String, String> mad) {
 //        StringBuilder tableDiscrepancies = new StringBuilder();
-        StringBuilder absences = new StringBuilder();
+        List<String> lineList = new ArrayList<String>();
 
         for (Map.Entry<String, String> entry : impact.entrySet()) {
             String student = entry.getKey();
@@ -23,7 +22,7 @@ public class DiscrepancyDetector {
                 if(!val2.equals(val)) {
                     String appendLine = student + " is " + val + " in IMPACT but "
                             + val2 + " in Monthly Attendance Report\n";
-                    absences.append(appendLine);
+                    lineList.add(appendLine);
                 }
             }
             else if(mad.containsKey(studentNoMiddleName)) {
@@ -31,7 +30,7 @@ public class DiscrepancyDetector {
                 if(!val2.equals(val)) {
                     String appendLine = studentNoMiddleName + " is " + val + " in IMPACT but "
                             + val2 + " in Monthly Attendance Report\n";
-                    absences.append(appendLine);
+                    lineList.add(appendLine);
                 }
             }
 //            else {
@@ -48,6 +47,17 @@ public class DiscrepancyDetector {
 //        }
 
 //        return tableDiscrepancies.toString() + absences.toString();
-        return absences.toString();
+        
+//        return absences.toString();
+        return sortAbsensesAlphabetically(lineList);
+    }
+
+    private static String sortAbsensesAlphabetically(List<String> absences) {
+        Collections.sort(absences);
+        StringBuilder sb = new StringBuilder();
+        for(String str : absences) {
+            sb.append(str);
+        }
+        return sb.toString();
     }
 }
--- Main.java
+++ Main.java
@@ -2,48 +2,77 @@ package com.mark.gubatan;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Main {
 
     // Change date before each run
-    private static final String date = "052316";
+    private static final String date = "011917";
+    private static final String month = "January";
+    private static final String schoolYear = "2016-2017";
 
     // Change downloadPath before first run
-    private static final String downloadPath = "C:/Users/Mark/Google Drive/Attendance/" + date + "/";
+    private static final String attendance = "TeamingSolution Attendance";
+    private static final String downloadPath = "C:/Users/DoomBot/Google Drive/YCCS Campus Data/";
 
-    private static final String[] schools = {"YCLA", "ADA", "Chatham", "Sullivan", "Latino", "JaneAddams", "CYDI",
-            "OHMC", "Truman", "ECA", "CCA", "ASA", "Campos", "Innovations", "WestTown", "WestSide", "APHS", "Charles"};
+    private static Map<String, String> schoolMap = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("504", "Ada McKinley");
+        schoolMap.put("506", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+        schoolMap.put("500", "YCLA");
+
+
         final long startTime = System.currentTimeMillis();
-        for(String cur : schools) {
-            runReports(cur);
+        for(Map.Entry<String, String> cur : schoolMap.entrySet()) {
+            runReports(cur.getKey(), cur.getValue());
         }
         final long endTime = System.currentTimeMillis();
         System.out.println("Execution finished in: " + ((endTime - startTime)/1000.0) + " seconds.");
     }
 
-    private static void runReports(String cur) {
+    private static void runReports(String id, String val) {
+        String path = downloadPath + id + "- " + val + "/" + attendance + "/" + schoolYear + "/" + month + "/";
+
         try {
             PDFManager pdfManager = new PDFManager();
             //IMPACT
-            pdfManager.setFilePath(downloadPath + "IMPACT_" + cur + "_" + date + ".pdf");
+            pdfManager.setFilePath(path + "Impact/IMPACT_" + val.replaceAll("\\s+","") + "_" + date + ".pdf");
             String impactPDF = pdfManager.ToText();
             HashMap<String, String> impact = ImpactParser.parseIMPACT(impactPDF);
 
             //POWER SCHOOL
-            pdfManager.setFilePath(downloadPath + "Monthly_Attendance_" + cur + "_" + date + ".pdf");
+            pdfManager.setFilePath(path + "PowerSchool/Monthly_Attendance_" + val.replaceAll("\\s+","") + "_" + date + ".pdf");
             String madPDF = pdfManager.ToText();
             HashMap<String, String> mad = PowerschoolParser.parseMonthlyAttendanceReport(madPDF);
 
             String differences = DiscrepancyDetector.getDiscrepancies(impact, mad);
 
-            PrintWriter writer = new PrintWriter(downloadPath + cur + "_" + date + ".txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("C:/Users/Doombot/1337hacking/" + val + "_" + date + ".txt", "UTF-8");
             writer.println(differences);
             writer.close();
         }
         catch (IOException e) {
             // Probably due to a file not existing (Does the name match the school in the school list?)
+            System.out.println(e.getMessage());
         }
     }
 
